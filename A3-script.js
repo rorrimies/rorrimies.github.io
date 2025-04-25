277c7873c181da86cf7b648f2c3077c42444f519
rorrimies <inna.quijano@gmail.com>
2025-04-25 18:25:43 -0400

    Update A3-script.js

--- A3-script.js
+++ A3-script.js
@@ -2,11 +2,11 @@ var silly = document.getElementById("silly");
 console.log(silly);
 
 function DivWasClicked(){
-    if(silly.innerHTML === "Click me if you need help looking for groceries :)"){
+    if(silly.innerHTML === "Click me if you need help looking for groceries :]"){
         silly.innerHTML = "Sorry haha, I have no clue! XD Ask an employee maybe?";
     }
     else{
-        silly.innerHTML = "Click me if you need help looking for groceries :)";
+        silly.innerHTML = "Click me if you need help looking for groceries :]";
     }   
 }
 
